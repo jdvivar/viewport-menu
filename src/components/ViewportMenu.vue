af4de6203bda4b13169c0c60096f924ce93bdb08
Daniel Vivar <jose.daniel.vivar.personat@ing.com>
2019-11-14 14:22:56 +0100

    Make it a web component

--- src/components/ViewportMenu.vue
+++ src/components/ViewportMenu.vue
@@ -3,13 +3,13 @@
     <div class="toolbar-item">
       <label>Size</label>
       <span class="input-group-width">
-        <input class="width-number" type="number" min="0" :max="max" step="1" v-model="width">
+        <input class="width-number" type="number" min="0" :max="max" step="1" v-model="width" />
         <span class="input-unit">px</span>
       </span>
     </div>
     <div class="toolbar-item">
       <label>Scale</label>
-      <input class="scale" type="number" min="0" max="3" step="0.1" v-model="scale">
+      <input class="scale" type="number" min="0" max="3" step="0.1" v-model="scale" />
     </div>
     <div class="toolbar-item">
       <label>Presets</label>
@@ -20,7 +20,6 @@
         @click="setViewport(preset.size, preset.scale)"
       >{{ preset.text }}</button>
     </div>
-
     <input
       class="width-range"
       type="range"
@@ -29,53 +28,60 @@
       :max="max"
       step="1"
       v-model="width"
-      :style="{ width: `${max}px` }"
-    >
-    <div
-      class="container"
-      :style="{ width: `${width}px`, transformOrigin: 'top left', transform: `scale(${ scale })`}"
-    >
-      <h1>hola</h1>
-    </div>
+      :style="{ 
+        width: `${ max }px`,
+        transform: `scale(${ scale })`,
+        transformOrigin: 'top left',
+        }"
+    />
   </div>
 </template>
 
 <script>
-export default {
-  data: function() {
-    return {
-      width: 200,
-      max: 1280,
-      scale: 1,
-      presets: [
-        { text: "iPhone 4/5/SE", size: 320, scale: 1 },
-        { text: "iPhone 6/7/8", size: 375, scale: 1 },
-        { text: "iPad", size: 768, scale: 1 },
-        { text: "Desktop (scaled)", size: 1280, scale: 0.5 }
-      ]
-    };
-  },
-  mounted: function() {
-    this.handleResize();
-    window.addEventListener("resize", this.handleResize);
-  },
-  beforeDestroy: function() {
-    window.removeEventListener("resize", this.handleResize);
-  },
-  methods: {
-    handleResize(event) {
-      this.max = this.$el.clientWidth;
+  export default {
+    data: function() {
+      return {
+        width: 600,
+        max: 1280,
+        scale: 1,
+        presets: [
+          { text: "iPhone 4/5/SE", size: 320, scale: 1 },
+          { text: "iPhone 6/7/8", size: 375, scale: 1 },
+          { text: "iPad", size: 768, scale: 1 },
+          { text: "Desktop (scaled)", size: 1280, scale: 0.5 }
+        ]
+      };
     },
-    setViewport(width, scale) {
-      this.width = width;
-      this.scale = scale;
+    watch: {
+      width: function(width) {
+        document.dispatchEvent(new CustomEvent('viewport:change', { detail: { width: width, scale: this.scale }}))
+      },
+      scale: function(scale) {
+        document.dispatchEvent(new CustomEvent('viewport:change', { detail: { width: this.width, scale }}))
+        this.handleResize()
+      }
+    },
+    mounted: function() {
+      this.$nextTick(this.handleResize)
+      window.addEventListener("resize", this.handleResize);
+    },
+    beforeDestroy: function() {
+      window.removeEventListener("resize", this.handleResize);
+    },
+    methods: {
+      handleResize() {
+        this.max = this.$el.clientWidth / this.scale;
+      },
+      setViewport(width, scale) {
+        this.width = width;
+        this.scale = scale;
+      }
     }
-  }
-};
+  };
 </script>
 
 <style>
-body {
+:host {
   font-family: Helvetica;
 }
 
@@ -95,12 +101,6 @@ label {
   position: relative;
 }
 
-.container {
-  background: green;
-  height: 100px;
-  margin-top: 5px;
-}
-
 .input-unit {
   font-size: 12px;
   position: absolute;
@@ -117,9 +117,8 @@ button.preset {
   outline: none;
 }
 
-button.preset:hover,
-button.preset:focus {
-  border-color: blue;
+input {
+  outline: none;
 }
 
 input[type="number"] {
@@ -128,6 +127,7 @@ input[type="number"] {
   padding: 10px 0 10px 10px;
   border: 1px solid #ddd;
   margin-right: 10px;
+  transition: all 0.5s ease;
 }
 
 input[type="range"].width-range {
@@ -147,9 +147,17 @@ input[type="range"].width-range::-webkit-slider-runnable-track {
 input[type="range"].width-range::-webkit-slider-thumb {
   cursor: pointer;
   -webkit-appearance: none;
-  border: 1px solid #333;
+  border: 1px solid #666;
   margin-top: -8px;
   width: 1px;
   height: 15px;
 }
+
+button.preset:hover,
+button.preset:focus,
+input:hover,
+input:focus {
+  border-color: #666;
+}
+
 </style>
--- src/main.js
+++
@@ -1,8 +0,0 @@
-import Vue from "vue";
-import ViewportMenu from "./ViewportMenu.vue";
-
-Vue.config.productionTip = false;
-
-new Vue({
-  render: h => h(ViewportMenu)
-}).$mount("#viewport-menu");
